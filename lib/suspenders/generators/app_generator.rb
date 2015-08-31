dc21b56e297ee6fe31f786d5739471005cfa421a
Damir Svrtan <damir.svrtan@gmail.com>
2015-08-31 10:10:25 +0200

    Remove bitters/bourbon/neat/refills

--- lib/suspenders/generators/app_generator.rb
+++ lib/suspenders/generators/app_generator.rb
@@ -28,8 +28,6 @@ module Suspenders
       invoke :create_suspenders_views
       invoke :configure_app
       invoke :setup_stylesheets
-      invoke :install_bitters
-      invoke :install_refills
       invoke :copy_miscellaneous_files
       invoke :customize_error_pages
       invoke :remove_routes_comment_lines
@@ -129,16 +127,6 @@ module Suspenders
       build :setup_stylesheets
     end
 
-    def install_bitters
-      say 'Install Bitters'
-      build :install_bitters
-    end
-
-    def install_refills
-      say "Install Refills"
-      build :install_refills
-    end
-
     def setup_git
       say 'Initializing git'
       invoke :setup_gitignore
@@ -191,7 +179,7 @@ module Suspenders
       say "Remember to run 'rails generate airbrake' with your API key."
     end
 
-    protected
+    private
 
     def get_builder_class
       Suspenders::AppBuilder
